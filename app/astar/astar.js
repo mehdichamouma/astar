6e53ff2a3bf0ef605206bfce90485527d771a234
Mehdi Chamouma <mehdi.chamouma@gmail.com>
2016-01-16 19:53:08 +0100

    comments and cleanup

--- app/astar/astar.js
+++ app/astar/astar.js
@@ -1,40 +1,78 @@
+/**
+* astar.js
+* Implémentation de l'algorithme A* en Javascript
+* Autheurs: Mehdi CHAMOUMA et François BEIGER
+*/
+
+//Importation module: arbre binaire
 var BinarySearchTree = require('binary-search-tree').BinarySearchTree;
 
-var aExplorerConstructor = function() {
+/**
+* Fonction node
+* Arguments: x, y, CoutSource, CoutDest, predecesseur
+* Renvoie un objet noe
+*/
+var node = function(x, y, CoutSource, CoutDest, predecesseur){
   return {
-    size: 0,
+    x: x,
+    y: y,
+    coutSource: CoutSource,
+    coutDest: CoutDest,
+    predecesseur: predecesseur,
+    estVisite: false
+  }
+}
 
-    sommetsNonVisites: new BinarySearchTree({compareKeys: (a,b) => (a<=b ? -1 : 1)}),
 
-    coordsIndexTree: new BinarySearchTree({
-      compareKeys: (a,b) => {
-            if(a.x < b.x) {
-              return -1
-            }
-            else if(a.x > b.x) {
-              return 1
-            }
-            else {
-              if(a.y<b.y) {
-                return -1;
+/**
+* Renvoie une structure de donnée permettant de dérouler l'algorithme A* (trouverChemin)
+*
+*
+*/
+var aExplorerConstructor = function() {
+
+  return {
+      /**
+      * Arbre binaire trié par Coût Final, contenant la liste à jour des sommets non visités
+      */
+      sommetsNonVisites: new BinarySearchTree({compareKeys: (a,b) => (a<=b ? -1 : 1)}),
+
+      /**
+      * Arbre binaire trié par Coordonnées (x, y), contenant la liste des sommets à explorés et explorés
+      */
+      coordsIndexTree: new BinarySearchTree({
+        compareKeys: (a,b) => {
+              if(a.x < b.x) {
+                return -1
               }
-              else if(a.y>b.y) {
-                return 1;
+              else if(a.x > b.x) {
+                return 1
               }
               else {
-                return 0;
+                if(a.y<b.y) {
+                  return -1;
+                }
+                else if(a.y>b.y) {
+                  return 1;
+                }
+                else {
+                  return 0;
+                }
               }
-            }
-            return 0;
-          },
-      unique: true
-  }),
+              return 0;
+            },
+        unique: true
+    }),
 
+    /**
+    * Insère un noeud si il n'a pas encore été visité, ou s'il n'est pas dans la liste des sommets à explorer
+    * Met à jour un noeud si il n'a pas encore été visité, et qu'il est dans la liste des sommets à explorer mais avec un coût supérieur
+    * Ne fait rien sinon
+    */
     insererSiNonVisite: function(node) {
       var shouldInsert = true;
       var m = this.coordsIndexTree.search({x: node.x, y: node.y})[0];
       var newCoutFinal = node.coutSource + node.coutDest;
-      //console.log(newCoutFinal, node.x, node.y);
       if(m) {
         var oldCoutFinal = m.coutSource + m.coutDest;
         if(!m.estVisite && (newCoutFinal < oldCoutFinal)) {
@@ -46,20 +84,22 @@ var aExplorerConstructor = function() {
         }
       }
       if(shouldInsert) {
-        if(node.x == 2 && node.y == 12) {
-          console.log("buuuuh");
-        }
         this.sommetsNonVisites.insert(newCoutFinal, node);
         this.coordsIndexTree.insert({x: node.x, y:node.y}, node);
       }
-      else {
-      }
     },
 
-    hasSommetAExplorer: function(){
+    /**
+    * Test s'il y a des sommets à explorer
+    */
+    hasSommetsAExplorer: function(){
       return  this.sommetsNonVisites.data.length != 0;
     },
 
+    /**
+    * Renvoie le noeud ayant un coût estimé minimal et l'enlève de la liste
+    * Cette fonction indique aussi que le noeud à été visité
+    */
     pop: function() {
       var minNode = this.sommetsNonVisites
       while(minNode.left!=null) {
@@ -75,12 +115,10 @@ var aExplorerConstructor = function() {
       //minNode = Racine
       else {
         if(minNode.right) {
-          console.log(minNode);
           this.sommetsNonVisites = minNode.right;
           minNode.right.parent = null;
         }
         else {
-          console.log(minNode);
           this.sommetsNonVisites = new BinarySearchTree({compareKeys: (a,b) => (a<=b ? -1 : 1)});
         }
       }
@@ -88,29 +126,31 @@ var aExplorerConstructor = function() {
       return minNode.data[0];
     },
 
+    /**
+    * Renvoie le prédecesseur d'un noeud en fonction de sa position
+    */
     recupPredecesseur: function(x,y){
       var precNode = this.coordsIndexTree.search({x: x, y: y});
       return precNode[0].predecesseur;
     }
-
   }
 }
 
-var node = function(x, y, CoutSource, CoutDest, predecesseur){
-  return {
-    x: x,
-    y: y,
-    coutSource: CoutSource,
-    coutDest: CoutDest,
-    predecesseur: predecesseur,
-    estVisite: false
-  }
-}
 
+/**
+* Fonction cheminHeuristique
+* Arguments: sourceX, sourceY, destX, destY
+* Calcule et renvoie la distance de Manhattan entre (sourceX, sourceY, destX, destY)
+*/
 var cheminHeuristique = function(sourceX, sourceY, destX, destY){
   return 5*(Math.abs(sourceX - destX) + Math.abs(sourceY - destY))
 }
 
+/**
+* Fonction getSuccessorsCoords
+* Arguments: x, y
+* Renvoie le tableau des successeurs de (x,y)
+*/
 var getSuccessorsCoords = function(x, y){
   return [
     [x, y-1],
@@ -120,6 +160,10 @@ var getSuccessorsCoords = function(x, y){
   ]
 }
 
+/**
+* Renvoie le coût en fonction du type de case
+*
+*/
 var getCost = function(type){
   switch(type){
     case 'h':
@@ -137,55 +181,57 @@ var getCost = function(type){
   }
 }
 
+
+/**
+* Function trouverChemin
+* Arguments: sourceX, sourceY, destX, destY, map
+* Trouve le chemin le plus court en utilisant l'algorithme A*
+*/
 var trouverChemin = function(sourceX, sourceY, destX, destY, map){
   //Contient la liste des cases à explorer
   var aExplorer = aExplorerConstructor();
 
+  //Mettre à vrai quand on est arrivé a destination
   var finish = false
 
+  //Taille de la map
   var tailleX = map[0].length
   var tailleY = map.length
+
+  //Initialisation
   var coutDest = cheminHeuristique(sourceX, sourceY, destX, destY)
   var coutSource = 0
   var source = node(sourceX, sourceY, coutSource, coutDest, null)
-
   aExplorer.insererSiNonVisite(source);
 
-  while(!finish){
-
+  while(aExplorer.hasSommetsAExplorer() && !finish){
+    //On recuperer le sommet ayant le cout minimum
+    //La fonction assure aussi qu'on ne récuperera pas le sommet une deuxième fois
     var X = aExplorer.pop()
-    console.log(X.x, X.y, X.coutSource, X.coutDest);
+
+    //On parcours ses successeurs et on les ajoute dans les sommets à explorer (s'ils n'ont pas été déjà visité)
     var successeurs = getSuccessorsCoords(X.x, X.y)
     successeurs.map(function(S){
       if(S[0] >= 0 && S[1] >= 0 && S[0] < tailleX && S[1] < tailleY) {
         coutSource = X.coutSource + getCost(map[S[1]][S[0]])
         coutDest = cheminHeuristique(S[0], S[1], destX, destY)
-        if(S[0] == 2 && S[1] == 12) {
-          console.log(aExplorer);
-          console.log("2, 12", coutSource, coutDest);
-        }
         aExplorer.insererSiNonVisite(node(S[0], S[1], coutSource, coutDest, [X.x, X.y]))
 
+        //On a finit si la destination est un des successeurs
         if(S[0]==destX && S[1]==destY){
           finish=true
         }
       }
-      else {
-        console.log("out of bound", S[0], S[1]);
-      }
     });
-    //finish=true;
-
   }
-  //console.log(aExplorer);
 
+  //On récupère et renvoie le chemin
   var p = [destX, destY]
   var chemin = []
   while(p!=null){
     chemin.push(p)
     p = aExplorer.recupPredecesseur(p[0], p[1])
   }
-  console.log(chemin);
   return chemin;
 }
 
